8e88dd3f07fa42acfa5f309891ed7ae63faa64f4
Edri Seitlli <icsd18193@icsd.aegean.gr>
2024-03-17 22:44:24 +0200

    host

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/general/header/header.component';
+import { FooterComponent } from './components/general/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { IntroComponent } from './components/home/intro/intro.component';
+import { AboutComponent } from './components/home/about/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    IntroComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
