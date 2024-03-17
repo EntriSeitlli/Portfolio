00de80a47c5eee0744ed71a3b2a1b3310b62acc4
Edri Seitlli <icsd18193@icsd.aegean.gr>
2024-03-17 23:21:29 +0200

    golive

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WUYEMRV5.mjs";import"./chunk-WLN7TQU2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HPLJ7YJT.mjs";import"./chunk-WLN7TQU2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
